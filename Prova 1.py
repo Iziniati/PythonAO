eea8a50db923d75b1358dd6532231e3d3023031d
Iziniati <ipons21201@iesjoanramis.org>
2025-11-12 13:18:59 +0100

    1

---
+++ Prova 1.py
@@ -0,0 +1,7 @@
+a= float(input("Escriure el primer nombre"))
+b= float(input("Escriure el segon nombre"))
+d= float(input("Escriu el tercer nombre"))
+c= a + b + d
+print("El resultat de la suma {} + {} + {} és {}".format(a, b, d, c))
+c = a * b * d
+print("El resultat de la multiplicació {} * {} * {} és {}".format(a, b, d, c))
