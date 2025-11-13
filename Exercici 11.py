185bddc0bf034ba7c4104ef10452a03180f9894f
Iziniati <ipons21201@iesjoanramis.org>
2025-11-13 11:25:07 +0100

    Ex11

---
+++ Exercici 11.py
@@ -0,0 +1,3 @@
+N=input("Escriu el teu nom: ")
+C=input("Escriu el teu cognom: ")
+print("Hola {} {}, com estas?".format(N, C))
---
+++ Exercici 12.py
@@ -0,0 +1,5 @@
+E = input("Escriu la teva edad: ")
+if E>18:
+    print("Ets major d'edad")
+else E<18:
+    Print:("Ets menor d'edad")
