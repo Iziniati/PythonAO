763221846890f2786b6f0928d13ff8943cc8b7cb
Iziniati <ipons21201@iesjoanramis.org>
2025-11-24 09:51:02 +0100

    Ex18

--- Exercici 18.py
+++ Exercici 18.py
@@ -1,4 +1,12 @@
 "Definir una funció que agafi un caràcter i retorni vertader si és una vocal i en cas contrari retorni fals. Prova-la amb diferents exemples."
 
 def ex18(c):
-    
+    v = "aeiouAEIOUàáèéìíòóùúÀÁÈÉÌÍÒÓÙÚ"
+    if c in v:
+        return True
+    else:
+        return False
+
+# Programa principal
+c = input("Escriu un caracter per a provar si es vocal o no")
+print(ex18(c))
