6a7e74f904c160045db57631ed2349672a13efb6
Iziniati <ipons21201@iesjoanramis.org>
2025-11-24 08:50:34 +0100

    prova 4

--- Prova 4.py
+++ Prova 4.py
@@ -1,5 +1,8 @@
 a = [1,3,5,7, "a", "Capça", [2,3,4]]
 for e in a:
     print(e)
-for i in len(a):
-    print(i)
+for i in range(len(a)):
+    a[i]*=2 # --> a[i]= a[i]*2
+    print("la posició {} té el valor {}". format(i,a[i]))
+for i,e in enumerate(a):
+    print("La posicio {} té el valor {}".format(i,e))
