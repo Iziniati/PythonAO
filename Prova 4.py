ad4387ed76a1915161d8acf2d0a38b05b8a20340
Iziniati <ipons21201@iesjoanramis.org>
2025-11-24 09:35:24 +0100

    Prova 4

--- Prova 4.py
+++ Prova 4.py
@@ -1,8 +1,36 @@
-a = [1,3,5,7, "a", "Capça", [2,3,4]]
-for e in a:
-    print(e)
+# a = [1, "a", "capça"[2], 1, "a"]
+a = [10, 9, 8, 7, 6, 5, 1, 2, 3 ,4]
+# Pasar els elements de la llista a string
 for i in range(len(a)):
-    a[i]*=2 # --> a[i]= a[i]*2
-    print("la posició {} té el valor {}". format(i,a[i]))
-for i,e in enumerate(a):
-    print("La posicio {} té el valor {}".format(i,e))
+    a[i]=str([i])
+# Crear un unic string separat per guio
+print("-",join(a))
+
+
+
+
+"""
+print(a[::-1]) # Retorna una llista invertida, però no modifica l'original
+print(a)
+print(a.reverse()) # No retorna res, però modifica la llista original
+print(a)
+
+a.sort()
+print(a)
+for i in range(len(a)):
+    a[i]=str(a(i))
+a.sort()     
+print(e)
+
+c= "capça" in a
+print(c)
+c = a.pop(2)
+print(c)
+a[0]= "Hola, som en Ramis"
+del a [:]
+a.append(10)
+a.append("cadena nova")
+a.append([10,11,12])
+a.extend((10,"cadena nova",[10,11,12]))
+print(a)
+"""
