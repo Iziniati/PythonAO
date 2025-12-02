40dfefddd2e3bc3ccd6b753015fc678a9bb129ce
Iziniati <ipons21201@iesjoanramis.org>
2025-12-02 10:34:25 +0100

    Prova 8

--- Prova 8.py
+++ Prova 8.py
@@ -1,4 +1,10 @@
-
+def factorial(n):
+    if n>0:
+        return n*factorial(n-1)
+    else:
+        return 1
+#Programa principal
+a = int(input("introdueix un nombre per fer el factorial"))
 
 
 
