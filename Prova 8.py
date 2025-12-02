d3ab8dce4b45f364be42db1eddb9301424e99a59
Iziniati <ipons21201@iesjoanramis.org>
2025-12-02 09:29:50 +0100

    Prova 8

--- Prova 8.py
+++ Prova 8.py
@@ -1,5 +1,14 @@
 v1 = int(input("intro el 1r numero: "))
 v2 = int(input("intro el 2n numero: "))
+
+
+
+
+
+"""
+v1 = int(input("intro el 1r numero: "))
+v2 = int(input("intro el 2n numero: "))
 r = (v1*v2) //2 
 for i in range(r, -1, -1):
     print(i)
+"""
