fbac8952d1e607ae219e22264314528fb723077f
Iziniati <ipons21201@iesjoanramis.org>
2025-12-16 11:11:53 +0100

    Prova examen

---
+++ Provaexamen.py
@@ -0,0 +1,51 @@
+# Ex 7
+
+
+
+
+"""
+# Ex 6
+edad = int(input("Introdueix la teva edad: "))
+if edad > 18:
+    print("Ets major d'edad")
+elif edad < 18:
+    print("Ets menor d'edad")
+else:
+    print("Tens 18 anys")
+if edad%2==0:
+    print("La teva edad es parell")
+else:
+    print("La tenva edad es senar")
+
+# Ex 5
+edad = int(input("Introdueix la teva edad: "))
+if edad > 18:
+    print("Ets major d'edad")
+elif edad < 18:
+    print("Ets menor d'edad")
+else:
+    print("Tens 18 anys")
+
+# Ex 4 
+edad = int(input("Introdueix la teva edad: "))
+print("Hola, tens {} anys".format(edad))
+
+
+# Ex3
+nom = input("Escriu el teu nom: ")
+b = "No"
+if "a" in a or "e":
+    b = "Si"
+print("Hola {} el teu nom té {} lletres i {} té la lletra a ".format(nom,len(a)c))
+
+
+# Ex 2
+nom = input("Escriu el teu nom: ")
+letres = len(nom)
+print("Hola, {}, el teu nom té {} lletres".format(nom, len(nom)))
+
+
+# Ex1
+nom = input("Escriu el teu nom: ")
+print("Hola, {}".format(nom))
+"""
